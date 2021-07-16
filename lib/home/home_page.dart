f7288bef118862926d449149d6a3d0d714c6bde2
dan7sc <su7cheng@gmail.com>
2021-07-16 19:17:38 -0300

    Increment state clicking on floating button

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_count_controller.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
@@ -8,8 +10,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeCountController();
+
+  @override
+  void initState() {
+    controller.listen((_) => setState(() {}));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("COUNT: ${controller.state}"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
